f587d3cc91a84068dcc31d9328794e91483af257
Merenga1986 <trash.ners@gmail.com>
2026-04-26 20:31:15 +0300

    - задание по бинарному поиску

---
+++ .idea/src/HW_FIRST/Main.java
@@ -0,0 +1,15 @@
+package HW_first;
+
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println("Repinin Maksim");
+        // Задание 2
+        int income;
+        int spending;
+        income = 44000;
+        spending = 30000;
+        System.out.println("Итого (руб)");
+        System.out.println(income - spending);
+    }
+}
--- .idea/src/binsearch/Main.java
+++ .idea/src/binsearch/Main.java
@@ -1,3 +1,5 @@
+package binsearch;
+
 public class Main {
 
     public static void main(String[] args) {
