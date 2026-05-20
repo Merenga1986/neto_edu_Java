cb4b044f605e56841d6434466decdbbb64f8af58
Merenga1986 <trash.ners@gmail.com>
2026-05-20 22:25:09 +0300

    - задание array

---
+++ src/algo/hash/LazyString.java
@@ -0,0 +1,63 @@
+package algo.hash;
+
+public class LazyString {
+    private String source;
+    private int start, end;
+    private int hash;
+
+    private LazyString() {}
+
+    public LazyString(String source, int start, int end) {
+        this.source = source;
+        this.start = start;
+        this.end = end;
+
+        // Считаем хеш как сумму кодов символов подстроки — O(n)
+        for (int i = start; i < end; i++) {
+            hash += source.charAt(i);
+        }
+    }
+
+    public LazyString shiftRight() {
+        LazyString shifted = new LazyString();
+        shifted.source = source;
+        shifted.start = start + 1;
+        shifted.end = end + 1;
+
+        // Вычитаем выпавший символ слева, прибавляем новый символ справа — O(1)
+        shifted.hash = hash - source.charAt(start) + source.charAt(end);
+
+        return shifted;
+    }
+
+    public int length() {
+        return end - start;
+    }
+
+    public boolean equals(LazyString that) {
+        if (length() != that.length()) {
+            return false;
+        }
+        for (int i = 0; i < length(); i++) {
+            char myChar = source.charAt(start + i);
+            char thatChar = source.charAt(that.start + i);
+            if (myChar != thatChar) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LazyString that = (LazyString) o;
+        return this.equals(that);
+    }
+}
---
+++ src/algo/hash/Main.java
@@ -0,0 +1,34 @@
+package algo.hash;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String source = "CACABABABCCCAABAC";
+
+        System.out.println(hasRepeats(source, 4)); // true
+        System.out.println(hasRepeats(source, 5)); // false
+    }
+
+    public static boolean hasRepeats(String source, int size) {
+        Set<LazyString> slices = new HashSet<>();
+        LazyString prev = null;
+        for (int i = 0; i <= source.length() - size; i++) {
+            LazyString slice;
+            if (prev == null) {
+                slice = new LazyString(source, i, i + size);
+            } else {
+                slice = prev.shiftRight();
+            }
+            if (slices.contains(slice)) {
+                return true;
+            } else {
+                slices.add(slice);
+            }
+            prev = slice;
+        }
+        return false;
+    }
+}
